8f694548410b08fef2e6ef4091997f3f05556f07
Maksim G. <golotmaks@gmail.com>
2026-05-03 21:57:33 +0300

    feat: function getAverage

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -51,8 +51,9 @@ function getCircleCircumference(radius) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  const bigIntNumberCount = 2n;
+  return Number((BigInt(value1) + BigInt(value2)) / bigIntNumberCount);
 }
 
 /**
@@ -70,8 +71,11 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const dx = x2 - x1;
+  const dy = y2 - y1;
+  const distanceBetweenPoints = Math.hypot(dx, dy);
+  return distanceBetweenPoints;
 }
 
 /**
