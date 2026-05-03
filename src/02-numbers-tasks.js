bbd435404806f4b0354a8805336d361e2c156941
Maksim G. <golotmaks@gmail.com>
2026-05-03 21:53:17 +0300

    feat: function getRectangleArea

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -18,8 +18,9 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height) {
+  const rectangleArea = width * height;
+  return rectangleArea;
 }
 
 /**
