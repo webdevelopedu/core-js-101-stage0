5103564e289d5c775bbe3d2b00d1bc985a3e391f
Maksim G. <golotmaks@gmail.com>
2026-05-04 16:14:48 +0300

    feat: function toNaryString

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -306,8 +306,9 @@ function isBracketsBalanced(/* str */) {
  *    365, 4  => '11231'
  *    365, 10 => '365'
  */
-function toNaryString(/* num, n */) {
-  throw new Error('Not implemented');
+function toNaryString(num, n) {
+  const naryString = Number(num).toString(n);
+  return naryString;
 }
 
 /**
