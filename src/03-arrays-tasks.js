8e18df3ce1bb7bd836083334eed9c3cb3d8132a7
Maksim G. <golotmaks@gmail.com>
2026-05-04 19:57:03 +0300

    feat: function distinct

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -469,8 +469,10 @@ function getIntervalArray(/* start, end */) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  const arraySet = new Set(arr);
+  const distinctArray = [...arraySet];
+  return distinctArray;
 }
 
 /**
