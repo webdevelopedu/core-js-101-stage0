e3cbd9905d65a63369525a74fdd4b837092690c6
Maksim G. <golotmaks@gmail.com>
2026-05-04 18:28:21 +0300

    feat: function doubleArray

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -50,8 +50,9 @@ function generateOdds(/* len */) {
  *    [0, 1, 2, 3, 4, 5] => [0, 1, 2, 3, 4, 5,   0, 1, 2, 3, 4, 5]
  *    [] => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  const doubledArray = arr.concat(arr);
+  return doubledArray;
 }
 
 /**
