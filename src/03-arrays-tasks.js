4341b59dda1d9978cb617de8387964b1b6b012ff
Maksim G. <golotmaks@gmail.com>
2026-05-04 19:46:22 +0300

    feat: function toStringList

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -383,8 +383,10 @@ function findAllOccurrences(/* arr, item */) {
  *    [1, 2, 3, 4, 5]                   => '1,2,3,4,5'
  *    ['rock', 'paper', 'scissors']     => 'rock,paper,scissors'
  */
-function toStringList(/* arr */) {
-  throw new Error('Not implemented');
+function toStringList(arr) {
+  const delimiter = ',';
+  const stringList = arr.join(delimiter);
+  return stringList;
 }
 
 /**
