fd4fe1724b2f659ba266bb8a45802550d84d95e4
Maksim G. <golotmaks@gmail.com>
2026-05-04 05:40:03 +0300

    feat: function isString

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -251,8 +251,9 @@ function encodeToRot13(/* str */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  const stringTypeLabel = '[object String]';
+  return Object.prototype.toString.call(value) === stringTypeLabel;
 }
 
 /**
