0f2334444b665e907543bf52f33593aca835d60c
Maksim G. <golotmaks@gmail.com>
2026-05-06 22:52:53 +0300

    feat: function reverseInteger

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -244,8 +244,8 @@ function reverseString(str) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  return parseInt(num.toString().split('').reverse().join('')) * Math.sign(num);
 }
 
 /**
