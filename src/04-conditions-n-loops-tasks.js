18145afaa1cfc8cc2d17ee28d8a6f9a69e2efb6e
Maksim G. <golotmaks@gmail.com>
2026-05-07 00:11:33 +0300

    feat: function getDigitalRoot

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -306,8 +306,9 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  if (num === 0) return 0;
+  return num % 9 === 0 ? 9 : num % 9;
 }
 
 /**
