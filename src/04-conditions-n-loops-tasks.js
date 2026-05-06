541ee6d086ebe738908dcda6753712f8f5d6b8f1
Maksim G. <golotmaks@gmail.com>
2026-05-06 22:22:32 +0300

    feat: function getIntervalString

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -198,8 +198,12 @@ function findFirstSingleChar(/* str */) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+  const startBracket = (a <= b ? isStartIncluded : isEndIncluded) ? '[' : '(';
+  const endBracket = (a <= b ? isEndIncluded : isStartIncluded) ? ']' : ')';
+  return `${startBracket}${min}, ${max}${endBracket}`;
 }
 
 /**
