c3a7fd95d8d6ba5b37a9dfd0726e067902109f72
Maksim G. <golotmaks@gmail.com>
2026-05-06 21:20:27 +0300

    feat: function getSumBetweenNumbers

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -60,8 +60,11 @@ function getFactorial(/* n */) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2) {
+  const start = Math.min(n1, n2);
+  const end = Math.max(n1, n2);
+  if (start === end) return start;
+  return start + getSumBetweenNumbers(start + 1, end);
 }
 
 /**
