dfe1da33e629e6ac8310d902b1773db32c03dcdf
Maksim G. <golotmaks@gmail.com>
2026-05-06 21:53:14 +0300

    feat: function isInsideCircle

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -153,8 +153,12 @@ function doRectanglesOverlap(rect1, rect2) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const dx = point.x - circle.center.x;
+  const dy = point.y - circle.center.y;
+  const distanceSquared = dx * dx + dy * dy;
+  const radiusSquared = circle.radius * circle.radius;
+  return distanceSquared < radiusSquared;
 }
 
 /**
