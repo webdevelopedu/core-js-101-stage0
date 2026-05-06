b62ed478bb4b305bc884262bf79f47db0c9bc6eb
Maksim G. <golotmaks@gmail.com>
2026-05-06 21:41:31 +0300

    feat: function doRectanglesOverlap

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -119,8 +119,12 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  const xLeft = Math.max(rect1.left, rect2.left);
+  const xRight = Math.min(rect1.left + rect1.width, rect2.left + rect2.width);
+  const yTop = Math.max(rect1.top, rect2.top);
+  const yBottom = Math.min(rect1.top + rect1.height, rect2.top + rect2.height);
+  return xLeft < xRight && yTop < yBottom;
 }
 
 /**
