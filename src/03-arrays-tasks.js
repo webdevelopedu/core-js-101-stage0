2cede2feef10bc59520a6be6c84fba065b11d3fa
Maksim G. <golotmaks@gmail.com>
2026-05-07 18:13:10 +0300

    feat: function generateOdds

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -34,8 +34,8 @@ function findElement(arr, value) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return new Array(len).fill(0).map((_, i) => 2 * i + 1);
 }
 
 /**
