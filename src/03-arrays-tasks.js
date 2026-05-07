fadb480c5519c38d4dd2fbebe1546d00139c7a3a
Maksim G. <golotmaks@gmail.com>
2026-05-07 18:31:14 +0300

    feat: function getMovingSum

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -234,8 +234,11 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  return arr.reduce((acc, x, i) => {
+    acc.push(i === 0 ? x : acc[i - 1] + x);
+    return acc;
+  }, []);
 }
 
 /**
