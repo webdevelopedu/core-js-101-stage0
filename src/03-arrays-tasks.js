5c395705e9b37be51ea58ab9c270e2dee5cc37b8
Maksim G. <golotmaks@gmail.com>
2026-05-07 19:18:40 +0300

    feat: function getPositivesCount

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -304,8 +304,8 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  return arr.filter(Number.isFinite).filter((n) => n > 0).length;
 }
 
 /**
