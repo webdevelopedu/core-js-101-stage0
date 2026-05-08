b2c14f0ab053cd04b2f50f8c6cdb709e73d64890
Maksim G. <golotmaks@gmail.com>
2026-05-08 21:47:14 +0300

    feat: function getIdentityMatrix

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -452,8 +452,10 @@ function sortCitiesArray(/* arr */) {
  *           [0,0,0,1,0],
  *           [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }).map((_, i) =>
+    Array.from({ length: n }).map((__, j) => (i === j ? 1 : 0))
+  );
 }
 
 /**
