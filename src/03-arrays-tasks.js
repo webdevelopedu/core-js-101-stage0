da5da234b8ad4aa00968f316b13febc1d0297777
Maksim G. <golotmaks@gmail.com>
2026-05-08 20:56:56 +0300

    feat: function getItemsSum

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -349,8 +349,8 @@ function sortDigitNamesByNumericOrder(arr) {
  *   [ -1, 1, -1, 1 ]      => 0
  *   [ 1, 10, 100, 1000 ]  => 1111
  */
-function getItemsSum(/* arr */) {
-  throw new Error('Not implemented');
+function getItemsSum(arr) {
+  return arr.reduce((sum, current) => sum + current, 0);
 }
 
 /**
